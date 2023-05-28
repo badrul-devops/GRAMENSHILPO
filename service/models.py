8eda9c4d939f75ea3b399a87f579e7cc59ba5873
Badrul_alam <badrulalam0123@gmail.com>
2023-05-28 22:15:25 +0600

    hello

--- service/models.py
+++ service/models.py
@@ -1,10 +1,1 @@
-from django.db import models
-
-# Create your models here.
-class catagoery(models.Model):
-    name=models.CharField(max_length=150)
-
-class sub_catagoery(models.Model):
-    name=models.CharField(max_length=150)
-    catagoery=models.ForeignKey(catagoery,on_delete=models.CASCADE)
-     
+ 
